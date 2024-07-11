1bf27fda0201a9b406fcf6b77f89cbb45df16179
Martín Santamaría <martinsantamaria19@gmail.com>
2024-07-11 15:22:59 -0300

    Re organización menú principal

--- app/Console/Commands/CreatePermissions.php
+++ app/Console/Commands/CreatePermissions.php
@@ -39,7 +39,7 @@ class CreatePermissions extends Command
                     'view_all' => false,
                 ],
                 [
-                  'slug' => 'raw-materials-management',
+                  'slug' => 'manufacturing',
                   'view_all' => false,
                 ],
                 [
@@ -136,6 +136,18 @@ class CreatePermissions extends Command
                 ],
                 [
                     'slug' => 'point-of-sale',
+                    'view_all' => false,
+                ],
+                [
+                    'slug' => 'sales-commerce',
+                    'view_all' => false,
+                ],
+                [
+                    'slug' => 'users',
+                    'view_all' => false,
+                ],
+                [
+                    'slug' => 'user-accounts',
                     'view_all' => false,
                 ],
             ]
